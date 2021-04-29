7d16739321c4812210cae774e4c3bdbaa8716167
pbishal <pbishal60@gmail.com>
2021-04-29 22:00:21 +0530

    Welcome to User registration

---
+++ UserRegistrationTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+        }
+    }
+}
